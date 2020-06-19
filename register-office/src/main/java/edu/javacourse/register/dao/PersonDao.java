dc9aacfbb0c8bec1df4e2af267a6de2c301d8b56
Rustem <reiz.rustem@gmail.com>
2020-06-19 22:44:48 +0300

    Lesson 120

--- register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -5,6 +5,7 @@ import edu.javacourse.register.domain.Person;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 public class PersonDao {
@@ -16,7 +17,9 @@ public class PersonDao {
         entityManager = factory.createEntityManager();
     }
 
-    public List<Person> findPerson() {
-        return entityManager.createQuery("SELECT p FROM Person p").getResultList();
+    public List<Person> findPersons() {
+        Query query = entityManager.createNamedQuery("Person.findPersons");
+        query.setParameter("personId", 1L);
+        return query.getResultList();
     }
 }
